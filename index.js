336a0bfbbb6b19094aa362861ab7e255ba11133e
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-02-27 10:55:43 -0500

    commit tree d734d4d57971b42dcc987ed3ed07b746cb8e28df

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function countDown(num) {
+    while (num >= 0) {
+       console.log(num);
+       num --;
+    }
+}
+
+const newArray = [];
+
+function writeCards(array, eventName) {
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newArray;
+};
